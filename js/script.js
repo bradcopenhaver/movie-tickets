76d3442d06c615dbc6333f97ceb06e3bf0c208fe
Brad Copenhaver and Eric Raetz <eric@>
2016-11-01 11:56:41 -0700

    add movie surcharge

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
-function Ticket(movieTitle, age, time) {
+function Ticket(movieTitle, age, time, surcharge) {
 	this.movieTitle = movieTitle;
 	this.age = age;
 	this.time = time;
+	this.surcharge = surcharge;
 }
-function Movie(title, showtime) {
+function Movie(title, showtime, quality) {
 	this.title = title;
 	this.showtime = showtime;
+	this.quality = quality;
 }
 
 var ageRange = function(age) {
-	if (age <= 18) {
+	if (age < 18) {
 		return "junior";
 	} else if (age >= 65) {
 		return "senior";
@@ -27,14 +29,17 @@ var ticketPrice = function(ticket) {
 	if (ticket.age === "junior" || ticket.age === "senior") {
 		price = price * 0.9;
 	}
+	if (ticket.surcharge === "premium") {
+		price = price * 1.5;
+	}
 	return price;
 }
 
-var movie1 = new Movie("Ben Hur", [1430, 1600, 1800]);
-var movie2 = new Movie("Happy Feet", [1230, 1400, 1530]);
-var movie3 = new Movie("Ernest Goes to Jail", [1600, 1800, 2000]);
-
-var movies = [movie1, movie2, movie3]
+var movie1 = new Movie("Ben Hur", [1430, 1600, 1800], 'nonpremium');
+var movie2 = new Movie("Happy Feet", [1230, 1400, 1530], 'nonpremium');
+var movie3 = new Movie("Ernest Goes to Jail", [1600, 1800, 2000], 'nonpremium');
+var movie4 = new Movie("Titanic", [1600, 1800, 2000], 'premium');
+var movies = [movie1, movie2, movie3, movie4]
 
 
 
@@ -47,7 +52,7 @@ $(document).ready(function() {
 		$(".clearMe").each(function(){
 			this.remove();
 		});
-		for (var i = 0; i < movies.length; i++) {
+		for (var i = 0; i < movies[parseInt($("#movie").val())].showtime.length; i++) {
 			$("#time").append("<option class='clearMe'>" + movies[parseInt($("#movie").val())].showtime[i] + "</option>");
 
 		$("#time").show();
@@ -55,15 +60,13 @@ $(document).ready(function() {
 	});
 	$("form").submit(function(event) {
 		event.preventDefault();
-		debugger;
-		var age = parseInt($("#age").val());
+				var age = parseInt($("#age").val());
 		var ticketAge = ageRange(age);
 
 		var time = parseInt($("#time").val())
 		var movieTitle = movies[parseInt($("#movie").val())].title
-
-		var ticket = new Ticket(movieTitle, ticketAge, time);
-		debugger;
+		var surcharge = movies[parseInt($("#movie").val())].quality
+		var ticket = new Ticket(movieTitle, ticketAge, time, surcharge);
 		$("#output").text(ticketPrice(ticket));
 	});
 });
